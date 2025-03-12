19ac9f66697cff738d6fc49959ac84899c9b3ced
ysko0114 <rhdudtn12345@gmail.com>
2025-03-12 19:10:58 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
-import(
-        "net/http"
-        "github.com/gin-gonic/gin"
+import (
+    "github.com/gin-gonic/gin"
+    "net/http"
 )
 
 func main() {
-        r := gin.Default()
-        r.GET("ping", func(c *gin.Context){
-                c.JSON(http.StatusOK, gin.H{
-                        "message":"hhellohihi",
-                }
-        })
-        r.Run(":9000")
+    r := gin.Default()
+
+    // 이미지가 있는 "images" 폴더를 웹에서 접근할 수 있도록 설정
+    r.Static("/images", "./images")
+
+    // 웹 페이지를 보여주는 부분
+    r.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.html", nil)
+    })
+
+    r.Run(":9000")
 }
