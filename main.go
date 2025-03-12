1b9a56a56f10261b061ff97eb933053412f7b42d
ysko0114 <rhdudtn12345@gmail.com>
2025-03-12 19:24:14 +0900

    Update main.go

--- main.go
+++ main.go
@@ -8,12 +8,19 @@ import (
 func main() {
         r := gin.Default()
 
+        // 프록시 설정
+        r.SetTrustedProxies([]string{"192.168.1.0/24", "10.0.0.0/8"})
+
         // HTML 템플릿 로드
-        r.LoadHTMLGlob("*.html") // 현재 디렉터리의 모든 HTML 파일 로드
-        // or r.LoadHTMLFiles("index.html") // 특정 HTML 파일 로드
+        r.LoadHTMLGlob("*.html")
 
         r.GET("/", func(c *gin.Context) {
                 c.HTML(http.StatusOK, "index.html", nil)
+        })
+
+        // 라우팅 정의
+        r.GET("/testtest001", func(c *gin.Context) {
+                c.String(http.StatusOK, "testtest001 page")
         })
 
         r.Run(":9000")
