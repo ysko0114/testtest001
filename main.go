b729b9a16cbba514a12b0b15de45c4765518c7b4
ysko0114 <rhdudtn12345@gmail.com>
2025-03-12 19:27:13 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,27 +1,23 @@
 package main
 
 import (
-        "net/http"
-        "github.com/gin-gonic/gin"
+    "net/http"
+
+    "github.com/gin-gonic/gin"
 )
 
 func main() {
-        r := gin.Default()
+    r := gin.Default()
 
-        // 프록시 설정
-        r.SetTrustedProxies([]string{"192.168.1.0/24", "10.0.0.0/8"})
+    // 정적 파일 제공 설정
+    r.Static("/image", "./image")
 
-        // HTML 템플릿 로드
-        r.LoadHTMLGlob("*.html")
+    // HTML 템플릿 로드
+    r.LoadHTMLGlob("testtest001/*")
 
-        r.GET("/", func(c *gin.Context) {
-                c.HTML(http.StatusOK, "index.html", nil)
-        })
+    r.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.html", nil)
+    })
 
-        // 라우팅 정의
-        r.GET("/testtest001", func(c *gin.Context) {
-                c.String(http.StatusOK, "testtest001 page")
-        })
-
-        r.Run(":9000")
+    r.Run(":9000")
 }
