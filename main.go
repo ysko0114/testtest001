31d19fd19787c119ad370a2bc9abe72a2f10edfa
ysko0114 <rhdudtn12345@gmail.com>
2025-03-12 19:30:22 +0900

    Update main.go

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ func main() {
     // 정적 파일 제공 설정
     r.Static("/image", "./image")
 
-    // HTML 템플릿 로드
-    r.LoadHTMLGlob("testtest001/*")
+    // HTML 템플릿 로드 (경로 수정)
+    r.LoadHTMLGlob("testtest001/*") // templates 디렉터리에 템플릿 파일이 있는 경우
 
     r.GET("/", func(c *gin.Context) {
         c.HTML(http.StatusOK, "index.html", nil)
